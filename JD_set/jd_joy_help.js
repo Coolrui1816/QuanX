624a3fdf4f96439bc5070d7ade9a76c7d6821e3b
Cool_rui <56864168+Coolrui1816@users.noreply.github.com>
2020-12-22 09:44:14 +0800

    Update jd_joy_help.js

--- JD_set/jd_joy_help.js
+++ JD_set/jd_joy_help.js
@@ -2,8 +2,7 @@
 宠汪汪强制为别人助力（助力一个好友你自己可以获得30积分，一天上限是帮助3个好友，自己获得90积分，不管助力是否成功，对方都会成为你的好友）
 更新地址：https://raw.githubusercontent.com/Coolrui1816/QuanX/master/JD_set/jd_joy_help.js
 原作者地址：https://raw.githubusercontent.com/lxk0301/scripts/master/jd_joy_help.js
-更新时间：2020-10-20
-目前提供了304位好友的friendPin供使用。脚本随机从里面获取一个，助力成功后，退出小程序重新点击进去开始助力新的好友
+更新时间：2020-11-12
 欢迎大家使用 https://jdjoy.jd.com/pet/getFriends?itemsPerPage=20&currentPage=1 (currentPage=1表示第一页好友，=2表示第二页好友)
 提供各自账号列表的friendPin给我
 如果想设置固定好友，那下载下来放到本地使用，可以修改friendPin换好友(助力一好友后，更换一次friendPin里面的内容)
@@ -14,14 +13,10 @@
 ③如提示好友人气旺，说明此好友已满了三人助力，需重新进出小程序，重新进入来客有礼-宠汪汪。
 [MITM]
 hostname = draw.jdfcloud.com
-
 圈x
 [rewrite_local]
 ^https:\/\/draw\.jdfcloud\.com\/\/pet\/enterRoom\/h5\?reqSource=weapp&invitePin=.*+(&inviteSource=task_invite&shareSource=\w+&inviteTimeStamp=\d+&openId=\w+)?|^https:\/\/draw\.jdfcloud\.com\/\/pet\/helpFriend\?friendPin url script-request-header https://raw.githubusercontent.com/lxk0301/scripts/master/jd_joy_help.js
-
-
 你也可从下面链接拿好友的friendPin(复制链接到有京东ck的浏览器打开即可)
-
 https://jdjoy.jd.com/pet/getFriends?itemsPerPage=20&currentPage=1
 **/
 let url = $request.url
